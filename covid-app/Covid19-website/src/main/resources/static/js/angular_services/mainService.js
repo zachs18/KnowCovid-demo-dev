9c0853ce2d83f3aec30a28aae3f01138156e8b10
zachs18 <8355914+zachs18@users.noreply.github.com>
2023-04-13 21:44:38 -0500

    Update mainService.js

--- covid-app/Covid19-website/src/main/resources/static/js/angular_services/mainService.js
+++ covid-app/Covid19-website/src/main/resources/static/js/angular_services/mainService.js
@@ -2,7 +2,7 @@ app.service('mainService', function($http, $q, $location, $window) {
 
 	//local
     //var serverURL = "http://localhost:9300/Covid-19/";
-    var serverURL = "http://ec2-54-166-86-211.compute-1.amazonaws.com:9090/Covid-19";
+    var serverURL = "http://54.166.86.211:9090/Covid-19";
     
 	return {
 	   callPostRestAPI : function(url, data) {
